785f0d6788356104ebcd94f58187e7bf0aa7b66e
faiyamm <sofia.almaraz@uabc.edu.mx>
2025-12-06 12:31:10 -0800

    Initial Commit

---
+++ 103-Project/_03_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _03_ProjectApp.swift
+//  103-Project
+//
+//  Created by Fai on 06/12/25.
+//
+
+import SwiftUI
+
+@main
+struct _03_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
